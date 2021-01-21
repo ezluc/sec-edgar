e5e6d4d4ab1f0f5b7af983ef5a0643ed23a36d7d
ezluc <37089708+ezluc@users.noreply.github.com>
2021-01-21 10:51:03 -0600

    Update sec_edgar.py

--- sec_edgar.py
+++ sec_edgar.py
@@ -1,3 +1,4 @@
+# tool searching CIK or Series ID on SEC based on mutual funds symbols 
 import re
 import urllib
 import _pickle 
@@ -15,7 +16,7 @@ def getCIKs(TICKERS):
         #print(Page)
         CIK_RE = re.findall(r'CIK=(S\d{9})', Page) or "NULL"
         print(CIK_RE[0])
-tickers_sample = pd.read_csv("C:\\py-sec-edgar\\refdata\\National Ticker List.csv")
+tickers_sample = pd.read_csv("LIST.csv") # PATH TO custom list of symbols
 #print(tickers_sample)
 #print(tickers_sample.SYMBOL)
 symbols_list = list(tickers_sample["SYMBOL"])
@@ -23,4 +24,4 @@ symbols_list = list(tickers_sample["SYMBOL"])
 #getCIKs(symbols_list)
 getCIKs(symbols_list)
 #print(CIK_samples)
-#CIK_samples.to_csv(r'C:\py-sec-edgar\refdata\cik_samples.csv')
+#CIK_samples.to_csv(r'SAMPLES.CSV') # PATH TO SAVE DEST
